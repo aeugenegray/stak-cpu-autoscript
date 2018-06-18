74f20860a80943a4a17a676abaa5a20dd6fa1107
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-18 14:21:13 -0700

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x
+spawn ./xmr-stak --currency cryptonight_lite_v7 -o triton.clevery.xyz:9202 -u Tw1Wf4BYraTYsWMA7SRiQbTLHpzoVueedCsDBjXAF6tZC9Fjjw7s1pnhTfMjeAMcZJ7JMKipyfSTYdRiAAagUNae28hB14UFA -p x
 expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
 send -- "0\r"
 expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
