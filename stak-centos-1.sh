44f623757b9e716096f1b88b39d7b1d6e9c2249f
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-18 15:04:30 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -2,14 +2,15 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
+echo 'vm.nr_hugepages = 128' >> /etc/sysctl.conf
 source ~/.bashrc
+sysctl -p
 git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
 cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
 mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/xmr-stak/build/bin
 expect ./expect-2.sh
-sudo sysctl -w vm.nr_hugepages=128
 tmux
 trap "" 15
 ./xmr-stak --currency cryptonight_lite_v7 -o pool1.crypto-me.com:13333 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x
