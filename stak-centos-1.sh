f2516cffceaa17f6ef868ba98b53df22c63e2d0e
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-18 16:35:49 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -2,9 +2,7 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
-echo 'vm.nr_hugepages = 128' > ~/etc/sysctl.conf
 source ~/.bashrc
-sysctl -p
 git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
 cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
