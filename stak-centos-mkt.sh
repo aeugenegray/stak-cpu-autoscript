c86a6b3d38b61c988107d43b7f5be2ad9d9db2c6
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-20 12:56:25 -0700

    Update stak-centos-mkt.sh

--- stak-centos-mkt.sh
+++ stak-centos-mkt.sh
@@ -3,14 +3,14 @@ sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
-cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+git clone https://github.com/MarketCashME/XMR-Stak.gitt && mkdir XMR-stak/build
+cd XMR-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
-mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/xmr-stak/build/bin
+mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/XMR-stak/build/bin
 expect ./expect-2.sh
 tmux
 trap "" 15
-./xmr-stak --currency cryptonight_alloy -o minenice.newpool.pw:1100 -u AA6o9xqV3vbA7tWWAdicrdKQTbtXCMatDHPPeTm3bodkanqYC5xrtNgTTYfsJeubV2jBjiBx9U9TGCfqhTMjg7a6SfrA5te -p x
+./xmr-stak --currency cryptonight_alloy -o mkt.mineallcrypto.com:7636 -u Mm1vqDWSZ5e9wL9M4VQf9KfqyU2n62xXHbcVd9n6wz62ctrp4uBw4Ta4kaVqvwtN28bALLfJfpudM1ocnHEGGjhwLVNDDFs -p x
 
 
