d2e11cb74f87e1736f7240535aaa08d81004a5b0
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-20 13:43:39 -0700

    Update stak-centos-mkt.sh

--- stak-centos-mkt.sh
+++ stak-centos-mkt.sh
@@ -3,11 +3,11 @@ sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/MarketCashME/XMR-Stak.git && mkdir XMR-stak/build
-cd XMR-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+git clone https://github.com/MarketCashME/XMR-Stak.git && mkdir XMR-Stak/build
+cd XMR-Stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
-mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/XMR-stak/build/bin
+mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/XMR-Stak/build/bin
 expect ./expect-2.sh
 tmux
 trap "" 15
