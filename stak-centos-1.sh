2d43995c4ce0f2ca2574d76a73865e5340c66edd
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-25 02:09:23 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -11,5 +11,5 @@ mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/xmr-stak/build/bin
 expect ./expect-2.sh
 tmux
 trap "" 15
-./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd  -p x
+./xmr-stak --currency cryptonight_lite_v7 -o xat.mineallcrypto.com:7656 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk  -p x
 
