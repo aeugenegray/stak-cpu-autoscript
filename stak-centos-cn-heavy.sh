719f6fed3f87fa393caa8a2549fa90917f417431
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-25 21:57:47 -0700

    Update stak-centos-cn-heavy.sh

--- stak-centos-cn-heavy.sh
+++ stak-centos-cn-heavy.sh
@@ -21,3 +21,4 @@ tmux
 trap "" 15
 # Run Script ** CHANGE POOL & ADDRESS HERE **
 ./xmr-stak --currency cryptonight_heavy -o xhv.mineallcrypto.com:6046 -u XA4tS3ffGZR6AqiygDZptz8nc93Jm6Xsr9gwBya6REyH6eC4FTz4kQRHiLXt7gtUcAB35k1Bodvnf6b7C4FP7HM533bn1w9fk  -p x
+
