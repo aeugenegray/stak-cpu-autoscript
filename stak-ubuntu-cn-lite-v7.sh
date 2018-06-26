82414d60713e9aef838e2c3922c6883cd2e57394
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-25 21:58:16 -0700

    Update stak-ubuntu-cn-lite-v7.sh

--- stak-ubuntu-cn-lite-v7.sh
+++ stak-ubuntu-cn-lite-v7.sh
@@ -1,1 +1,16 @@
+#! /bin/bash
+sudo apt-get -y groupinstall "Development Tools"
+sudo apt-get install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
+echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
+source ~/.bashrc
+git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
+cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+make install
+cd bin
+mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/xmr-stak/build/bin
+expect ./expect-2.sh
+tmux
+trap "" 15
+./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
+
 
