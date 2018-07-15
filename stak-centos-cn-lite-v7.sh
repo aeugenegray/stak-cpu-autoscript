077ba76de422ee1463b549b565aeb4ac69c8097c
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-07-15 10:26:47 +0000

    Update stak-centos-cn-lite-v7.sh

--- stak-centos-cn-lite-v7.sh
+++ stak-centos-cn-lite-v7.sh
@@ -8,7 +8,7 @@ cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
 mv ~/stak-cpu-autoscript/expect-2.sh ~/stak-cpu-autoscript/xmr-stak/build/bin
-expect ./expect-2.sh
+expect ./expect-mg.sh
 tmux
 trap "" 15
 ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
