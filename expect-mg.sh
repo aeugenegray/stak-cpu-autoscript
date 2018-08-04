f2b2796da664050352a5ecf32e84e0fed332ee40
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-08-04 02:54:11 +0000

    Update expect-mg.sh

--- expect-mg.sh
+++ expect-mg.sh
@@ -5,41 +5,6 @@ send -- "0\r"
 expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
 send -- "\r"
 expect -re "Pool configuration stored in file 'pools.txt'\r"
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
-set timeout 240
-send \x03
-spawn ./xmr-stak --currency cryptonight_lite_v7 -o spot.mineallcrypto.com:7586 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd   -p x
+set timeout 12000
 expect eof
 
