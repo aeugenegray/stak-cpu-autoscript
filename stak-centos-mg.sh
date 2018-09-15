c6bc0caaed1f32214e72bf90c6902e422aa53f4a
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-09-15 04:44:15 +0000

    Update stak-centos-mg.sh

--- stak-centos-mg.sh
+++ stak-centos-mg.sh
@@ -20,11 +20,8 @@ expect ./expect-mg.sh
 /sbin/chkconfig crond on
 /sbin/service crond start
 echo '*/2 * * * * /sbin/shutdown -r' >> /etc/crontab
-echo '@reboot rm -rf /root/xmr-stak/build/bin' >> /etc/crontab
-echo '@reboot cp -rf /root/stak-cpu-autoscript/bin /root/stak-cpu-autoscript/xmr-stak/build/' >> /etc/crontab
-echo '@reboot tmux' >> /etc/crontab
-echo '@reboot trap "" 15' >> /etc/crontab
-echo '@reboot /root/stak-cpu-autoscript/xmr-stak/build/bin/xmr-stak --currency cryptonight -o pool.macscoin.site:3333 -u MgTJuJaFrLMGuGU7NcgzXJPxUvMNXwZX7QN16nr29u8iRGaa3HybJ9QVrPTyqTBiZka5zSVec55Ac8C4ofE78TaNTFfEcYw -p x' >> /etc/crontab
+echo '@reboot rm -rf /root/stak-cpu-autoscript/xmr-stak/build/bin' >> /etc/crontab
+echo '@reboot /root/stak-cpu-autoscript/cron.sh' >> /etc/crontab
 # Trap sigterm
 trap "" 15
 # Run Script ** CHANGE POOL & ADDRESS HERE **
