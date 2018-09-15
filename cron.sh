4c8a087bb339e40f2cdf127dcd1968a733ed5b68
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-09-15 21:06:40 +0000

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -3,3 +3,4 @@ cp -rf /root/stak-cpu-autoscript/bin/xmr-stak /root/stak-cpu-autoscript/xmr-stak
 tmux
 trap "" 15
 /root/stak-cpu-autoscript/xmr-stak/build/bin/xmr-stak --currency cryptonight -o recoal.my-mining-pool.de:22333 -u co9Bc5kTy2UGZatj1QKaJVDGtXk1wh2FPdE4whTk4Vx1XzTubdh9Tiz6A82uHBF91wAR6CY3Qwx4pHN4LC6swjez1KcR8tDhr -p x
+
