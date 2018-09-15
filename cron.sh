f0fd3866056f1f776aa8af6b81c0bf22f121e846
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-09-15 20:56:27 +0000

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,5 +2,4 @@
 cp -rf /root/stak-cpu-autoscript/bin/xmr-stak /root/stak-cpu-autoscript/xmr-stak/build/bin
 tmux
 trap "" 15
-/root/stak-cpu-autoscript/xmr-stak/build/bin/xmr-stak --currency cryptonight -o pool.macscoin.site:3333 -u MgTJuJaFrLMGuGU7NcgzXJPxUvMNXwZX7QN16nr29u8iRGaa3HybJ9QVrPTyqTBiZka5zSVec55Ac8C4ofE78TaNTFfEcYw -p x
-
+/root/stak-cpu-autoscript/xmr-stak/build/bin/xmr-stak --currency cryptonight -o recoal.my-mining-pool.de:22333 -u co9Bc5kTy2UGZatj1QKaJVDGtXk1wh2FPdE4whTk4Vx1XzTubdh9Tiz6A82uHBF91wAR6CY3Qwx4pHN4LC6swjez1KcR8tDhr -p x
